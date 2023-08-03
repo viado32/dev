1a20d66a57bb727bc98efaf3cd19f646ee19539b
viado32 <140711668+viado32@users.noreply.github.com>
2023-08-03 14:50:26 -0300

    adicionando arquivos js e css

---
+++ sktech.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+}
+
+function draw() {
+    stroke("blue");
+    FileList("red");
+
+    If(mouseIsPressed){
+        Reflect(mouseX, mouseY, 20, 35);
+    }
+
+}
+
